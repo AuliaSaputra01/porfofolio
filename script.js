7923c7988582d113b9bd0a5651e93f70c17daad1
AuliaSaputra01 <auliasaputr401@gmail.com>
2026-05-04 10:41:00 +0700

    Enhance dark mode toggle with icon change
    Update dark mode toggle functionality to change icon based on state.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const darkMode = document.getElementById("darkMode");
 
 darkMode.onclick = () => {
+
   document.body.classList.toggle("dark");
+
+  if(document.body.classList.contains("dark")){
+    darkMode.innerHTML = "☀️";
+  }else{
+    darkMode.innerHTML = "🌙";
+  }
+
 };
